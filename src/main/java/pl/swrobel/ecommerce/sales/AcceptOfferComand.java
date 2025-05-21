87d94731c2da65dda26f4857e62d2e450e08d625
seba1288 <seba128818@gmail.com>
2025-05-21 13:03:11 +0200

    added sales tests but needed fixes

--- src/main/java/pl/swrobel/ecommerce/sales/AcceptOfferComand.java
+++ src/main/java/pl/swrobel/ecommerce/sales/AcceptOfferComand.java
@@ -4,4 +4,31 @@ public class AcceptOfferComand {
     String fname;
     String lname;
     String email;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public AcceptOfferComand setFname(String fname) {
+        this.fname = fname;
+        return this;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public AcceptOfferComand setLname(String lname) {
+        this.lname = lname;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public AcceptOfferComand setEmail(String email) {
+        this.email = email;
+        return this;
+    }
 }
--- src/main/java/pl/swrobel/ecommerce/sales/SalesFacade.java
+++ src/main/java/pl/swrobel/ecommerce/sales/SalesFacade.java
@@ -3,9 +3,15 @@ package pl.swrobel.ecommerce.sales;
 import java.math.BigDecimal;
 
 public class SalesFacade {
-    public void addToCart( String productId, BigDecimal quantity) {}
+    public void addToCart( String customerId, String productId) {}
 
     public void acceptOffer(AcceptOfferComand acceptOfferComand) {}
 
+    public Offer getCurrentOffer(String customerId) {
+        return new Offer();
+    }
+
+
     public void makeReservationPaid(String reservatioId) {}
+
 }
